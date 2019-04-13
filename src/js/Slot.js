90b91471d0f29b3c97f5f28592780e9f4aa46e01
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-13 05:44:20 +0000

    Deploy johakr/html5-slot-machine to github.com/johakr/html5-slot-machine.git:gh-pages

---
+++ src/js/Slot.js
@@ -0,0 +1,69 @@
+import Reel from './Reel.js';
+import Symbol from './Symbol.js';
+
+export default class Slot {
+  constructor(domElement, config = {}) {
+    Symbol.preload();
+
+    this.currentSymbols = [
+      ['death_star', 'death_star', 'death_star'],
+      ['death_star', 'death_star', 'death_star'],
+      ['death_star', 'death_star', 'death_star'],
+      ['death_star', 'death_star', 'death_star'],
+      ['death_star', 'death_star', 'death_star'],
+    ];
+
+    this.nextSymbols = [
+      ['death_star', 'death_star', 'death_star'],
+      ['death_star', 'death_star', 'death_star'],
+      ['death_star', 'death_star', 'death_star'],
+      ['death_star', 'death_star', 'death_star'],
+      ['death_star', 'death_star', 'death_star'],
+    ]
+
+    this.container = domElement;
+
+    this.reels = Array.from(this.container.getElementsByClassName('reel')).map((reelContainer, idx) => new Reel(reelContainer, idx, this.currentSymbols[idx]));
+
+    this.spinButton = document.getElementById('spin');
+    this.spinButton.addEventListener('click', () => this.spin());
+
+    this.autoPlayCheckbox = document.getElementById('autoplay');
+
+    if (config.inverted) {
+      this.container.classList.add('inverted');
+    } 
+  }
+
+  spin() {
+    this.onSpinStart();
+
+    this.currentSymbols = this.nextSymbols;
+    this.nextSymbols = [
+      [Symbol.random(), Symbol.random(), Symbol.random()],
+      [Symbol.random(), Symbol.random(), Symbol.random()],
+      [Symbol.random(), Symbol.random(), Symbol.random()],
+      [Symbol.random(), Symbol.random(), Symbol.random()],
+      [Symbol.random(), Symbol.random(), Symbol.random()],
+    ];
+
+    return Promise.all(this.reels.map(reel => {
+      reel.renderSymbols(this.currentSymbols[reel.idx], this.nextSymbols[reel.idx]);
+      return reel.spin();
+    })).then(() => this.onSpinEnd());
+  }
+
+  onSpinStart() {
+    this.spinButton.disabled = true;
+
+    console.log('SPIN START');
+  }
+
+  onSpinEnd() {
+    this.spinButton.disabled = false;
+
+    console.log('SPIN END');
+
+    if (this.autoPlayCheckbox.checked) return window.setTimeout(() => this.spin(), 200);
+  }
+}
---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import Slot from './Slot.js';
+
+const config = {
+  inverted: false // true: reels spin from top to bottom; false: reels spin from bottom to top
+}
+
+const slot = new Slot(document.getElementById('slot'), config);
