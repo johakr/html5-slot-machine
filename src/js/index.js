82a05363cb7a9e4f0ab85a692dd644bea06017b1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-13 05:49:54 +0000

    Deploy johakr/html5-slot-machine to github.com/johakr/html5-slot-machine.git:gh-pages

--- src/js/index.js
+++
@@ -1,7 +0,0 @@
-import Slot from './Slot.js';
-
-const config = {
-  inverted: false // true: reels spin from top to bottom; false: reels spin from bottom to top
-}
-
-const slot = new Slot(document.getElementById('slot'), config);
